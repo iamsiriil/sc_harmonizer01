293693772403c29c459fe3d0b8f2bbc4894eee41
siriil <siriil@protonmail.ch>
2025-07-02 12:01:47 +0100

    update: harmony_01_parse

--- data/scdump/python/dt_harmony_01_parse.py
+++ data/scdump/python/dt_harmony_01_parse.py
@@ -1,70 +1,72 @@
 from music21 import *
 
 def parseSCDump():
-    file = open("./scdump/dump01.txt", "r")
-    voices = [[], [], [], []]
-    numVoices = int(file.readline()) 
+    with open("/home/siriil/Music/projects/harmony01/data/scdump/python/dump.txt", "r") as file:
+        chords = []
+        progSize = int(file.readline()) 
 
-    for n in range(0, numVoices):
-        notes = str(file.readline().strip("\n")) 
-        durations = str(file.readline().strip("\n")) 
-        voices[n].append(list(map(int, notes.strip("[]").split(", "))))
-        voices[n].append(list(map(int, durations.strip("[]").split(", "))))
+        for _ in range(0, progSize):
+            chords.append(str(file.readline()).strip(" \n").split(" "))
 
-    file.close
+        ciphers = []
+        rules = []
+        for _ in range(0, progSize):
+            rule = []
+            ciphers.append(str(file.readline()).strip(" \n"))
+            for _ in range(0, 14):
+                rule.append(str(file.readline()).strip(" \n"))
 
-    return voices
+            rules.append(rule)
 
-rules = []
-#parts = parseSCDump()
-#
-#score = stream.Score()
-#
-#treble = stream.Part()
-#bass = stream.Part()
-#
-#spn = stream.Voice()
-#alt = stream.Voice()
-#ten = stream.Voice()
-#bas = stream.Voice()
-#
-#for n in parts[0][0]:
-#    nt = note.Note()
-#    dur = duration.Duration(type='half')
-#    nt.pitch.midi = n
-#    nt.duration = dur
-#    nt.stemDirection = 'down'
-#    bas.append(nt)
-#
-#for n in parts[1][0]:
-#    dur = duration.Duration(type='half')
-#    nt = note.Note()
-#    nt.pitch.midi = n
-#    nt.duration = dur
-#    nt.stemDirection = 'up'
-#    ten.append(nt)
-#
-#for n in parts[2][0]:
-#    dur = duration.Duration(type='half')
-#    nt = note.Note()
-#    nt.pitch.midi = n
-#    nt.duration = dur
-#    nt.stemDirection = 'down'
-#    alt.append(nt)
-#
-#for n in parts[3][0]:
-#    dur = duration.Duration(type='half')
-#    nt = note.Note()
-#    nt.pitch.midi = n
-#    nt.duration = dur
-#    nt.stemDirection = 'up'
-#    spn.append(nt)
-#
-#bass.append(bas)
-#bass.append(ten)
-#treble.append([alt, spn])
-#
-#score.insert(0, treble)
-#score.insert(0, bass)
-#
-#score.show()
+    return progSize, ciphers, chords, rules
+
+
+size, ciphers, chords, rules = parseSCDump()
+
+#for i in range(0, size):
+    #print(ciphers[i])
+   # print(chords[i])
+   # for r in rules[i]:
+   #     print(r)
+def getScore(size, ciphers, chords, rules):
+    score = stream.Score()
+
+    treble = stream.Part()
+    bass = stream.Part()
+
+    bas = stream.Voice()
+    ten = stream.Voice()
+    alt = stream.Voice()
+    spn = stream.Voice()
+
+    voices = [bas, ten, alt, spn]
+
+    for v, i in zip(voices, range(0, 4)):
+        for c, j in zip(ciphers, range(0, size)):
+            stm = 'down' if i % 2 == 0 else 'up'
+            dur = duration.Duration(type='whole')
+            nt = note.Note(chords[j][i])
+
+            nt.duration = dur
+            nt.stemDirection = stm
+            v.append(nt)
+
+            if i == 0:
+                for k in rules[j]:
+                    rule = expressions.TextExpression(k)
+                    rule.style.fontSize = 5
+                    v.append(rule)
+
+
+
+
+    bass.append([bas, ten])
+    treble.append([alt, spn])
+
+    score.insert(0, treble)
+    score.insert(0, bass)
+
+    score.show()
+    
+
+getScore(size, ciphers, chords, rules)
